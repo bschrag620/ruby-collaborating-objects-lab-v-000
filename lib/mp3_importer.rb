028de87a2024813f520b01151be18b8abbbc5b52
Brad Schrag <brad.schrag@gmail.com>
2018-11-20 04:28:04 +0000

    automatically backed up by learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,13 +2,13 @@ class MP3Importer
   attr_accessor :path
 
   @@all = []
-  
+
   def initialize(path)
     @path = path
   end
-  
+
   def files
-    path = @path + '/*.mp3'  
+    path = @path + '/*.mp3'
     file_list = Dir[path]
     file_list.each do |file|
       @@all << file
