dec0e8f5381aae31c60947da82d9f33618f0ea3a
Brad Schrag <brad.schrag@gmail.com>
2018-11-20 04:40:40 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,7 +6,10 @@ class Song
   end
 
   def self.new_by_filename(file_name)
-    no_ext = file_name.chomp('.mp3')
-    Song.new(no_ext)
+    parts = file_name.chomp('.mp3').split(' - ')
+    new_song = Song.new(parts[1])
+    new_song.artist = Artist.find_or_create_by_name(parts[0])
+    new_sone.artist.add_song(new_song)
+    new_song  
   end
 end
